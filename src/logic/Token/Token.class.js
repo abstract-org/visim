42d4a6fc4774c66fe58cba8898ab5ef5442519bf
ivp5 <68510611+ivp5@users.noreply.github.com>
2022-08-04 17:16:25 +0300

    remove author, add pools and positions

--- src/logic/Token/Token.class.js
+++ src/logic/Token/Token.class.js
@@ -1,12 +1,16 @@
 import sha256 from 'crypto-js/sha256';
 
+// per figma mockup with octahedrons, no authors, just investors
+
 export default class Token {
     hash;
-    name;
-    authorHash;
-
-    constructor(name, authorHash) {
+    name; // potentially hashing to hash
+    //pools[tokenid] including initial usdc pool (using usdc tokenid)
+    //LP_positions[] all LP positions created by this token
+    
+    constructor(name) {
         this.name = name;
-        this.hash = '0x'+sha256(`${name} + ${authorHash}`);
+        //this.hash = '0x'+sha256(`${name}`);
+        
     }
 }
