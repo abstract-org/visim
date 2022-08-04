d35b88d5de245997ad1d8d9fe46926ceddb14568
ivp5 <68510611+ivp5@users.noreply.github.com>
2022-08-04 06:37:05 +0300

    simplify log/exponents, fix liquidity math funcs

--- src/logic/Pool/Pool.class.js
+++ src/logic/Pool/Pool.class.js
@@ -6,7 +6,7 @@ export default class Pool {
     token1;
     currentPrice = globalConfig.PRICE_MIN;
     sqrtCurrentPrice = Math.sqrt(globalConfig.PRICE_MIN);
-    currentTick = this.squarePriceToTick(Math.sqrt(globalConfig.PRICE_MIN));
+    currentTick = this.sqrtPriceToTick(Math.sqrt(globalConfig.PRICE_MIN));
     liquidity = 0;
     ticks = new Array(((globalConfig.TICK_MAX * 2) / globalConfig.TICK_SPACING) + 1).fill(0);
     tickSpacing = globalConfig.TICK_SPACING;
@@ -19,42 +19,35 @@ export default class Pool {
         this.hash = '0x'+sha256(`${token0} + ${token1}`);
     }
 
-    squareTickToPrice(sqrtTick) {
-        return Math.sqrt(Math.pow(1.0001, sqrtTick));
+    tickToSqrtPrice(tick) {    // sqrt is x^1/2
+        return 1.0001 ** tick/2; 
     }
 
-    squarePriceToTick(sqrtPrice) {
-        return (Math.floor((
-                Math.log(sqrtPrice ** 2) / Math.log(1.0001))
-                 / this.tickSpacing) * this.tickSpacing);
+    sqrtPriceToTick(sqrtPrice) {    // ln(x^2)/ln(1.0001) = ln(x)*2/ln(1.0001) = ln(x) * 2 * 10000.5 = ln(x)*20001
+        return ~~(Math.log(sqrtPrice)*20001 / this.tickSpacing) * this.tickSpacing;
     }
 
-    getLiquidityForAmounts(amount0, amount1, sqrtPriceMin, sqrtPriceMax) {
-        const liquidity0 = amount0 / (1 / sqrtPriceMin - 1 / sqrtPriceMax);
-        const liquidity1 = amount1 / (this.sqrtCurrentPrice - sqrtPriceMin);
-
-        if (this.sqrtCurrentPrice <= sqrtPriceMin) {
-            return liquidity0;
-        } else if (this.sqrtCurrentPrice < sqrtPriceMax) {
-            return Math.min(liquidity0, liquidity1);
-        }
-
-        return liquidity1;
+    getLiquidityForAmounts(amount0, amount1, sqrtPriceMin, sqrtPriceMax, curSqrtPrice) { 
+                // local curSqrtPrice so can do dry runs regardless of current pool price
+                // TODO: floor prices to nearest tick, or take ticks as parameters and calc price2tick outside (preferred)
+        
+        //clamp price to [min..max] range
+        clampedPrice = Math.max(Math.min(curSqrtPrice, sqrtPriceMax), sqrtPriceMin)
+        
+        return Math.min(   // min(num,inf)=num, should work
+                amount1 / (clampedPrice - sqrtPriceMin),  
+                amount0 / (1/clampedPrice - 1/sqrtPriceMax)
+        );
     }
 
-    getAmountsForLiquidity(sqrtPriceMin, sqrtPriceMax) {
-        const sqrtPrice0 = Math.max(Math.min(this.sqrtCurrentPrice, sqrtPriceMax), sqrtPriceMin);
-        const sqrtPrice1 = Math.max(Math.min(this.sqrtCurrentPrice, sqrtPriceMin), sqrtPriceMax);
-        const amount0 = this.liquidity * (1 / sqrtPrice0) - (1 / sqrtPriceMax);
-        const amount1 = this.liquidity * (sqrtPrice1 - sqrtPriceMin);
+    getAmountsForLiquidity(liquidity, tickMin, tickMax, curSqrtPrice) { // changed parameters to ticks, not prices, added liq and curprice
+        sqrtPriceMax=tickToSqrtPrice(tickMax);
+        sqrtPriceMin=tickToSqrtPrice(tickMin);
+        clampedPrice = Math.max(Math.min(curSqrtPrice, sqrtPriceMax), sqrtPriceMin) // clamps cur to [min..max] range 
+        const amountRight = liquidity * (1/clampedPrice - 1/sqrtPriceMax);
+        const amountLeft  = liquidity * (clampedPrice - sqrtPriceMin);
 
-        if (this.sqrtCurrentPrice <= sqrtPriceMin) {
-            return [amount0];
-        } else if (this.sqrtCurrentPrice < sqrtPriceMax) {
-            return [amount0, amount1];
-        }
-
-        return [amount1];
+        return [amountLeft, amountRight]; // one can be zero if current price outside the range
     }
 
     setTick(tick) {
