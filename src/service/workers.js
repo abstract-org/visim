069af3651048233b2bf0d5af9c5d58c248d71aba
Boris L <boris@agora-labs.com>
2022-10-17 14:44:08 -0700

    Removed web worker attempts

--- src/service/workers.js
+++
@@ -1,5 +0,0 @@
-export const smartSwap = (router, token0, token1, amt, depth) => {
-    //const [totalIn, totalOut] = router.smartSwap(token0, token1, amt, depth)
-
-    return JSON.stringify(router)
-}
--- src/service/workersLoader.js
+++
@@ -1,3 +0,0 @@
-import smartSwap from 'workerize-loader?inline!./workers' // eslint-disable-line import/no-webpack-loader-syntax
-
-export default smartSwap
