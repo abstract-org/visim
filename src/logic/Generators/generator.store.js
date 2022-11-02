ffdfb029da9ecd3922f0a2d377a8920d85ff5f44
ostapan <vitalik.ostapenko@gmail.com>
2022-11-02 20:14:02 +0200

    upd: added action names to zustand stores for better history view in ReduxDevTools

--- src/logic/Generators/generator.store.js
+++ src/logic/Generators/generator.store.js
@@ -16,67 +16,100 @@ const useGeneratorStore = create(
     devtools(
         (set, get) => ({
             ...INITIAL_STATE,
-            setScenarioId: (scenarioId) => set((state) => ({ scenarioId })),
+            setScenarioId: (scenarioId) =>
+                set((state) => ({ scenarioId }), false, 'setScenarioId'),
             addInvConfig: (stateConfig) =>
-                set((state) => ({
-                    invConfigs: [
-                        ...state.invConfigs,
-                        {
-                            ...stateConfig
-                        }
-                    ]
-                })),
+                set(
+                    (state) => ({
+                        invConfigs: [
+                            ...state.invConfigs,
+                            {
+                                ...stateConfig
+                            }
+                        ]
+                    }),
+                    false,
+                    'addInvConfig'
+                ),
             updateInvConfig: (stateConfig) =>
-                set((state) => ({
-                    invConfigs: state.invConfigs.map((gen) => {
-                        if (gen.invGenAlias === stateConfig.invGenAlias) {
-                            return {
-                                ...stateConfig
+                set(
+                    (state) => ({
+                        invConfigs: state.invConfigs.map((gen) => {
+                            if (gen.invGenAlias === stateConfig.invGenAlias) {
+                                return {
+                                    ...stateConfig
+                                }
+                            } else {
+                                return gen
                             }
-                        } else {
-                            return gen
-                        }
-                    })
-                })),
+                        })
+                    }),
+                    false,
+                    'updateInvConfig'
+                ),
             deleteInvConfig: (invGenAlias) =>
-                set((state) => ({
-                    invConfigs: state.invConfigs.filter(
-                        (gen) => gen.invGenAlias !== invGenAlias
-                    )
-                })),
-            resetInvConfigs: () => set((state) => ({ invConfigs: [] })),
+                set(
+                    (state) => ({
+                        invConfigs: state.invConfigs.filter(
+                            (gen) => gen.invGenAlias !== invGenAlias
+                        )
+                    }),
+                    false,
+                    'deleteInvConfig'
+                ),
+            resetInvConfigs: () =>
+                set((state) => ({ invConfigs: [] }), false, 'resetInvConfigs'),
             addQuestConfig: (stateConfig) =>
-                set((state) => ({
-                    questConfigs: [
-                        ...state.questConfigs,
-                        {
-                            ...stateConfig
-                        }
-                    ]
-                })),
-            updateQuestConfig: (stateConfig) =>
-                set((state) => ({
-                    questConfigs: state.questConfigs.map((gen) => {
-                        if (gen.questGenAlias === stateConfig.questGenAlias) {
-                            return {
+                set(
+                    (state) => ({
+                        questConfigs: [
+                            ...state.questConfigs,
+                            {
                                 ...stateConfig
                             }
-                        } else {
-                            return gen
-                        }
-                    })
-                })),
+                        ]
+                    }),
+                    false,
+                    'addQuestConfig'
+                ),
+            updateQuestConfig: (stateConfig) =>
+                set(
+                    (state) => ({
+                        questConfigs: state.questConfigs.map((gen) => {
+                            if (
+                                gen.questGenAlias === stateConfig.questGenAlias
+                            ) {
+                                return {
+                                    ...stateConfig
+                                }
+                            } else {
+                                return gen
+                            }
+                        })
+                    }),
+                    false,
+                    'updateQuestConfig'
+                ),
             deleteQuestConfig: (questGenAlias) =>
-                set((state) => ({
-                    questConfigs: state.questConfigs.filter(
-                        (gen) => gen.questGenAlias !== questGenAlias
-                    )
-                })),
-            resetQuestConfigs: () => set(() => ({ questConfigs: [] })),
+                set(
+                    (state) => ({
+                        questConfigs: state.questConfigs.filter(
+                            (gen) => gen.questGenAlias !== questGenAlias
+                        )
+                    }),
+                    false,
+                    'deleteQuestConfig'
+                ),
+            resetQuestConfigs: () =>
+                set(() => ({ questConfigs: [] }), false, 'resetQuestConfigs'),
             setNeedScrollUp: (needScrollUp) =>
                 set(() => ({ needScrollUp }), false, 'setNeedScrollUp'),
             override: (newData) =>
-                set(() => overrideState(get(), newData, INITIAL_STATE))
+                set(
+                    () => overrideState(get(), newData, INITIAL_STATE),
+                    false,
+                    'override'
+                )
         }),
         devToolsOptions
     )
--- src/logic/Pool/pool.store.js
+++ src/logic/Pool/pool.store.js
@@ -18,7 +18,11 @@ const usePoolStore = create(
         (set, get) => ({
             ...INITIAL_STATE,
             addPool: (pool) =>
-                set((state) => ({ pools: [...state.pools, pool] })),
+                set(
+                    (state) => ({ pools: [...state.pools, pool] }),
+                    false,
+                    'addPool'
+                ),
             addMultiplePools: (pools) =>
                 set(
                     produce((state) => {
@@ -27,10 +31,16 @@ const usePoolStore = create(
                                 state.pools.push(pool)
                             }
                         })
-                    })
+                    }),
+                    false,
+                    'addMultiplePools'
                 ),
             addSwap: (swap) =>
-                set((state) => ({ swaps: [...state.swaps, swap] })),
+                set(
+                    (state) => ({ swaps: [...state.swaps, swap] }),
+                    false,
+                    'addSwap'
+                ),
             addMultipleSwaps: (swaps) =>
                 set(
                     produce((state) => {
@@ -39,12 +49,20 @@ const usePoolStore = create(
                                 state.swaps.push(swap)
                             }
                         })
-                    })
+                    }),
+                    false,
+                    'addMultipleSwaps'
                 ),
-            setActive: (pool) => set(() => ({ active: pool })),
-            setSwapMode: (mode) => set((state) => ({ swapMode: mode })),
+            setActive: (pool) =>
+                set(() => ({ active: pool }), false, 'setActive'),
+            setSwapMode: (mode) =>
+                set((state) => ({ swapMode: mode }), false, 'setSwapMode'),
             override: (newData) =>
-                set(() => overrideState(get(), newData, INITIAL_STATE))
+                set(
+                    () => overrideState(get(), newData, INITIAL_STATE),
+                    false,
+                    'override'
+                )
         }),
         devToolsOptions
     )
